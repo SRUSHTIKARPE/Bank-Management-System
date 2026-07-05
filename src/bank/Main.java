02b3875c97c6c951c5599fb17e3bfe2aa6a5f193
codewithom007 <bhagatom0000@gmail.com>
2026-07-05 17:57:33 +0530

    Added create account method

--- src/bank/Main.java
+++ src/bank/Main.java
@@ -1,24 +1,69 @@
 package bank;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
         Bank bank = new Bank("ABC Bank","Sangamner");
+        System.out.println("Enter customer details : ");
 
-Customer customer1 = new Customer(
-1,
-"Snehal",
-"9876543210",
-"snehal@gmail.com",
-"Sangamner");
+        System.out.print("Customer ID:");
+        int customerId=sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("Name:");
+        String name=sc.nextLine();
+
+        System.out.print("Phone number:");
+        String phoneNumber=sc.nextLine();
+
+        System.out.print("Email:");
+        String email=sc.nextLine();
+
+        System.out.print("Address:");
+        String address=sc.nextLine();
+
+        System.out.println("Enter Account details :");
+        System.out.print("Account Number:");
+        int accountNumber=sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("Account Type:");
+        String accountType=sc.nextLine();
+
+        System.out.print("balance");
+        double balance=sc.nextDouble();
+
+        
+// Customer customer1 = new Customer(
+// 1,
+// "Snehal",
+// "9876543210",
+// "snehal@gmail.com",
+// "Sangamner");
+
+        Customer customer1 = new Customer(
+customerId,
+name,
+phoneNumber,
+email,
+address);
+
+
+// Account account1 = new Account(
+// 1001,
+// "Savings",
+// 5000,
+// customer1);
 
 Account account1 = new Account(
-1001,
-"Savings",
-5000,
+accountNumber,
+accountType,
+balance,
 customer1);
-
 bank.createAccount(account1);
 
 bank.displayAllAccounts();
